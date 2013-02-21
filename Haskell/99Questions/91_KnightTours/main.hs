fca6da0729e20547a610cc68a94614c96b922daf
GHScan <ppscan@qq.com>
2013-02-21 20:50:26 +0800

    Haskell/99Questions/91_KnightTours: Optimize

--- Haskell/99Questions/91_KnightTours/main.hs
+++ Haskell/99Questions/91_KnightTours/main.hs
@@ -1,18 +1,14 @@
 import Data.List
 
 main = putStrLn $ show output
-output = head $knightVisit 5 (1,1)
-
-isKnightJump dx dy = adx > 0 && adx + ady == 3
-    where 
-    (adx,ady) = (abs dx,abs dy)
+output = head $knightVisit 7 (1,1)
 
 knightVisit n start = visit [start]
     where
     grids = [(x,y)|x<-[1..n],y<-[1..n]]
-    jumps = [(p1,p2)|p1@(x1,y1)<-grids,p2@(x2,y2)<-grids,isKnightJump (x1-x2) (y1-y2)]
+    jumps = [(p1,p2)|p1@(x1,y1)<-grids,p2@(x2,y2)<-grids,x1/=x2,abs (x1-x2)+abs (y1-y2)==3]
     visit path
-        | length path == n * n = [path]
-    visit path = concatMap (\jump->visit$path ++ [snd jump]) validJumps
+        | length path == n * n = [reverse path]
+    visit path = concatMap (\p2->visit $p2:path) validNextPos
         where
-        validJumps = [jump|jump@(p1,p2)<-jumps,p1 == last path,all (/=p2) path]
+        validNextPos = [p2|(p1,p2)<-jumps,p1 == head path,not$elem p2 path]
