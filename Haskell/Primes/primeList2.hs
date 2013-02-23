904b57b13bb8434aeb5d300efd9e212cc3766eef
GHScan <ppscan@qq.com>
2013-02-23 23:57:48 +0800

    Haskell/Primes: Add primeList2

--- Haskell/Primes/primeList2.hs
+++ Haskell/Primes/primeList2.hs
@@ -2,7 +2,7 @@ import Data.List
 import qualified Data.Set as Set
 
 main = print output
-output = primeList 100
+output = length$primeList 10000
 
 primeList maxN = _primeList 2 maxN Set.empty
     where
@@ -12,3 +12,14 @@ primeList maxN = _primeList 2 maxN Set.empty
         where
         newExcl = foldr Set.insert excl $ takeWhile (<maxN) [i,i+i..]
         nextp = head [x|x<-[i+1..],odd x,not $Set.member x newExcl]
+
+replace v i l = left++v:right
+    where (left,_:right) = splitAt i l
+
+-- this is far slower than primeList
+primeList2 maxN = map fst$filter (\(i,b)->b) $zip [2..] (drop 2 $_primeList 2 $replicate maxN True)
+    where
+    _primeList i boolL  
+        | i >= maxN = boolL
+        | not$boolL !! i = _primeList (i+1) boolL
+        | boolL !! i = _primeList (i+1) $foldr (replace False) boolL $takeWhile (<maxN) [i*2,i*3..]
