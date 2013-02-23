8b064709786066358c9622c5096bf03fd7057c22
GHScan <ppscan@qq.com>
2013-02-23 18:20:41 +0800

    Misc\makefile_c++\Windows: Update

--- Misc/makefile_c++/Windows/make.bat
+++ Misc/makefile_c++/Windows/make.bat
@@ -1,6 +1,5 @@
 @echo off
 set PATH=F:\MinGW\bin;%PATH%
-call "%VS80COMNTOOLS%vsvars32.bat" >nul
 rem call "%VS90COMNTOOLS%vsvars32.bat" > nul
-rem call "%VS100COMNTOOLS%vsvars32.bat" > nul
+call "%VS110COMNTOOLS%vsvars32.bat" > nul
 make.exe %*
