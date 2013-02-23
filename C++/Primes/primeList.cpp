b6643712ec7118affbf47212ddc101edd21d5cb4
GHScan <ppscan@qq.com>
2013-02-24 03:46:52 +0800

    C++/Primes: Add primeList

---
+++ C++/Primes/primeList.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+
+#include <vector>
+
+vector<int> primeList(int maxN)
+{
+    vector<bool> boolL(maxN + 1, true);
+    for (int i = 2; i < maxN; ++i) {
+        if (boolL[i]) {
+            for (int j = i + i; j < maxN; j += i) {
+                boolL[j] = false;
+            }
+        }
+    }
+    vector<int> r;
+    for (int i = 2; i < maxN; ++i) if (boolL[i]) r.push_back(i);
+    return r;
+}
+
+int main()
+{
+    cout << primeList(2000000).size() << endl;
+}
