f2cffdf855c9cabc3b9a6b1b032a64a1bb549f7f
GHScan <ppscan@qq.com>
2013-02-24 03:47:45 +0800

    Python/Primes: Add primeList

---
+++ Python/Primes/primeList.py
@@ -0,0 +1,11 @@
+
+def primeList(maxN):
+    l = [True for i in range(maxN + 1)]
+    for i in range(2, maxN):
+        if not l[i]: pass
+        else:
+            for j in range(2*i,maxN,i):
+                l[j] = False
+    return [i for (i,b) in zip(range(2,maxN), l[2:]) if b]
+
+print len(primeList(2000000))
