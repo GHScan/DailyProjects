65ca6b52cf2452bca66aa6b22d5f0cdc8991ea27
GHScan <ppscan@qq.com>
2013-08-13 20:33:49 +0800

    CSAPP/3/3.39_full_umul: Rewrite the function mul64_cpp

--- CSAPP/3/3.39_full_umul/main.cpp
+++ CSAPP/3/3.39_full_umul/main.cpp
@@ -11,24 +11,23 @@ static void mul64_uint64(uint32_t result[2], uint32_t x, uint32_t y) {
 }
 
 static void mul64_cpp(uint32_t result[2], uint32_t x, uint32_t y) {
-    uint32_t lx = x & 0xffff, hx = x >> 16;
-    uint32_t ly = y & 0xffff, hy = y >> 16;
-    result[0] = lx * ly;
-    result[1] = hx * hy;
-    uint32_t t1 = lx * hy;
-    uint32_t t2 = hx * ly + t1;
-    uint32_t old_r0 = result[0];
-    result[0] += (t2 & 0xffff) << 16;
-    result[1] += t2 >> 16;
-    if (t2 < t1) {
-        result[1] += 1 << 16;
-    }
-    if (result[0] < old_r0) {
-        result[1] += 1;
-    }
-}
+    uint32_t lx = x & 0xffff; 
+    uint32_t hx = x >> 16;
+    uint32_t ly = y & 0xffff; 
+    uint32_t hy = y >> 16;
 
-static void mul64_asm(uint32_t result[2], uint32_t x, uint32_t y) {
+    uint32_t ll = lx * ly;
+    uint32_t lh = lx * hy;
+    uint32_t hl = hx * ly;
+    uint32_t hh = hx * hy;
+
+    result[0] = result[1] = 0;
+
+    result[0] += ll & 0xffff;
+    uint32_t t = (ll >> 16) + (lh & 0xffff) + (hl & 0xffff);
+    result[0] += (t & 0xffff) << 16;
+    result[1] += t >> 16;
+    result[1] += (lh >> 16) + (hl >> 16) + hh;
 }
 
 double test(void(*f)(uint32_t[2], uint32_t, uint32_t), uint32_t *inputs1, uint32_t *inputs2, int n) {
@@ -58,8 +57,10 @@ int main() {
     genInputs(inputs1, N);
     genInputs(inputs2, N);
 
+    puts("mul64_uint64");
     ret = test(mul64_uint64, inputs1, inputs2, N);
 
+    puts("mul64_cpp");
     ret2 = test(mul64_cpp, inputs1, inputs2, N);
     if (ret != ret2) printf("failed !: %f, %f\n", ret, ret2);
 
