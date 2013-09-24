d5e0b678493358b56428d6a875a6e558085ae8a6
GHScan <ppscan@qq.com>
2013-09-25 00:15:02 +0800

    Python/CompilerStudy/Lesson5_OperatorPrecedenceEval: Rewrite

--- Python/CompilerStudy/Lesson5_OperatorPrecedenceEval/main.py
+++ Python/CompilerStudy/Lesson5_OperatorPrecedenceEval/main.py
@@ -2,35 +2,34 @@
 
 import re
 
-PRIORS = {'+':1, '-':1, '*':2, '/':2, '%':2, ')':0}
+lbps = {'+':1, '-':1, '*':2, '/':2, '%':2, ')':0}
+rbps = lbps
+def _expr(tokens, rbp):
+    lv = _atom(tokens)
+    while len(tokens) > 0 and rbp < lbps[tokens[0]]:
+        lv = _infix(tokens, lv)
+    return lv
 def _atom(tokens):
-    if tokens[0] == '(':
-        tokens.pop(0)
-        r = _expr(tokens, 0)
-        tokens.pop(0)
-    else:
-        r = int(tokens[0])
-        tokens.pop(0)
-    return r
+    t = tokens.pop(0)
+    if t == '(':
+        ret = _expr(tokens, 0)
+        assert tokens.pop(0) == ')'
+        return ret
+    elif t == '-':
+        return - _expr(tokens, 5)
+    else: return int(t)
 def _infix(tokens, lv):
-    t = tokens[0]
-    tokens.pop(0)
-    rv = _expr(tokens, PRIORS[t])
-    if t == '+': return lv + rv
-    elif t == '-': return lv - rv
-    elif t == '*': return lv * rv
-    elif t == '/': return lv / rv
-    elif t == '%': return lv % rv
+    op = tokens.pop(0)
+    rv = _expr(tokens, rbps[op])
+    if op == '+': return lv + rv
+    elif op == '-': return lv - rv
+    elif op == '*': return lv * rv
+    elif op == '/': return lv / rv
+    elif op == '%': return lv % rv
     else: assert False
-def _expr(tokens, prior):
-    v = _atom(tokens)
-    while len(tokens) > 0 and prior < PRIORS[tokens[0]]:
-        v = _infix(tokens, v)
-    return v
-
 def myeval(src):
-    tokens = re.findall(r'\d+|\S', src)
-    return _expr(tokens, 0)
+    return _expr(re.findall('\d+|\S', src), 0)
 
-print myeval('5-2*(1+2)')
-print myeval('9-8+7*6% 5 -4 +3')
+assert myeval('5-2*(1+2)') == -1
+assert myeval('9-8+7*6% 5 -4 +3') == 2
+assert myeval('1+2--3+(-3+5*-1)') == -2
