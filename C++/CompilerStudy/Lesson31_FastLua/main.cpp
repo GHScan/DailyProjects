3ca0f6fbfc3afb960b16a790cb4570dd67e46132
GHScan <ppscan@qq.com>
2014-01-04 18:24:45 +0800

    C++/CompilerStudy/Lesson31_FastLua: Add

---
+++ C++/CompilerStudy/Lesson31_FastLua/main.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+
+#include <time.h>
+
+using namespace std;
+
+int main() {
+}
---
+++ C++/CompilerStudy/Lesson31_FastLua/pch.h
@@ -0,0 +1,13 @@
+
+#ifndef PCH_H
+#define PCH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#endif
