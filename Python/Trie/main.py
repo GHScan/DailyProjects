3d8d4871efb6e187dba442c601b413495cb032da
GHScan <wyyxscan@126.com>
2014-01-17 10:42:59 +0800

    Python/Trie: Refactory to oo

--- Python/Trie/main.py
+++ Python/Trie/main.py
@@ -1,32 +1,36 @@
 # vim:fileencoding=utf-8
 
-trieTree = dict()
-def insert(s):
-    for i in range(len(s)):
-        _insert(trieTree, s, i)
-def _insert(node, s, start):
-    for i in range(start, len(s) + 1):
-        if i > start:
-            node.setdefault('_res', set()).add(s)
-        if i < len(s):
-            node = node.setdefault(s[i:i+1], dict())
-def get(s):
-    n = trieTree
-    for i in range(len(s)):
-        n = n.get(s[i:i+1], None)
-        if not n: return None
-    return n.get('_res', None)
-def clear():
-    trieTree = dict()
+class Trie(object):
+    def __init__(self):
+        self.m_root = dict()
+    def insert(self, s):
+        for i in range(len(s)):
+            self._insert(self.m_root, s, i)
+    def _insert(self, node, s, start):
+        for i in range(start, len(s) + 1):
+            if i > start:
+                node.setdefault('_res', set()).add(s)
+            if i < len(s):
+                node = node.setdefault(s[i:i+1], dict())
+    def get(self, s):
+        n = self.m_root
+        for i in range(len(s)):
+            n = n.get(s[i:i+1], None)
+            if not n: return None
+        return n.get('_res', None)
+    def clear(self):
+        self.m_root = dict()
 
 import re, time
 
+t = Trie()
+
 start = time.clock()
 for w in re.findall(r'\w+', file('1.txt').read(1<<20)):
-    insert(w)
+    t.insert(w)
 print(time.clock() - start)
 
 while True:
     line = raw_input()
     if not line: break
-    print(get(line))
+    print(t.get(line))
