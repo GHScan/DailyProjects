42e52698d44edfcae5a7f0ad2d72666cc168b7f8
ghscan <ppscan@qq.com>
2014-03-11 14:34:45 +0800

    C++/TCPSocket: Refactor main.cpp

--- C++/TCPSocket/main.cpp
+++ C++/TCPSocket/main.cpp
@@ -1,23 +1,7 @@
-#define APPTYPE_BlockingClient      
-//#define APPTYPE_NonblockingClient   
-//#define APPTYPE_BlockingServer      
-//#define APPTYPE_ProactorServer      
-//#define APPTYPE_ProactorServer2     
-//#define APPTYPE_ProactorClient      
-//#define APPTYPE_WebProxy            
-
-#if defined(APPTYPE_BlockingClient)
 #include "BlockingClient/main.cpp"
-#elif defined(APPTYPE_NonblockingClient)
-#include "NonblockingClient/main.cpp"
-#elif defined(APPTYPE_BlockingServer)
-#include "BlockingServer/main.cpp"
-#elif defined(APPTYPE_ProactorServer)
-#include "ProactorServer/main.cpp"
-#elif defined(APPTYPE_ProactorServer2)
-#include "ProactorServer2/main.cpp"
-#elif defined(APPTYPE_ProactorClient)
-#include "ProactorClient/main.cpp"
-#elif defined(APPTYPE_WebProxy)
-#include "WebProxy/main.cpp"
-#endif
+//#include "NonblockingClient/main.cpp"
+//#include "BlockingServer/main.cpp"
+//#include "ProactorServer/main.cpp"
+//#include "ProactorServer2/main.cpp"
+//#include "ProactorClient/main.cpp"
+//#include "WebProxy/main.cpp"
