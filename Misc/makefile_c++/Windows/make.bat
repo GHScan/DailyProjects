2bd4b0eb6feb7a8b5a851e99aeb4eaea5a0bc31c
ghscan <ppscan@qq.com>
2014-04-02 17:10:48 +0800

    Misc/makefile_c++/Windows: Add -analyze option for static analyze

--- Misc/makefile_c++/Windows/make.bat
+++ Misc/makefile_c++/Windows/make.bat
@@ -1,5 +1,5 @@
 @echo off
 set PATH=F:\MinGW\bin;%PATH%
 rem call "%VS90COMNTOOLS%vsvars32.bat" > nul
-call "%VS110COMNTOOLS%vsvars32.bat" > nul
+call "%VS120COMNTOOLS%vsvars32.bat" > nul
 make.exe %*
