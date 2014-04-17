7409cf2ca51fbeab24eded858a761326cc2e5d7b
ghscan <ppscan@qq.com>
2014-04-17 23:06:17 +0800

    Python/UniqLineCounter: Refactor, read lines from stdin or open files from argv

--- Python/UniqLineCounter/main.py
+++ Python/UniqLineCounter/main.py
@@ -1,12 +1,15 @@
 import sys, os, subprocess, hashlib
 
-def getFileList(dirpath):
-    for dpath, dnames, fnames in os.walk(dirpath):
-        for fname in fnames:
-            yield os.path.join(dpath, fname)
+def getFileNames(path):
+    if os.path.isdir(path):
+        for dpath, dnames, fnames in os.walk(path):
+            for fname in fnames:
+                yield os.path.join(dpath, fname)
+    else:
+        yield path
 
-def getTextFileList(dirpath):
-    for fname in getFileList(dirpath):
+def filterTextFileNames(fnames):
+    for fname in fnames:
         if  subprocess.check_output(['file', fname]).find('text') != -1:
             yield fname
 
@@ -20,7 +23,12 @@ def str2MD5(s):
     m.update(s)
     return m.hexdigest()
 
+if __name__ != '__main__': exit(1)
+
 result = set()
-for dirpath in sys.argv[1:]:
-    result |= set(str2MD5(line) for line in catFiles(getTextFileList(dirpath)))
+if len(sys.argv) == 1:
+    result |= set(str2MD5(line) for line in sys.stdin.readlines())
+else:
+    for path in sys.argv[1:]:
+        result |= set(str2MD5(line) for line in catFiles(filterTextFileNames(getFileNames(path))))
 print len(result)
