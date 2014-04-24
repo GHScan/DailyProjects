baf52e5f2ffccf566de7b73f4dfb9da56c17ce98
ghscan <ppscan@qq.com>
2014-04-24 21:17:06 +0800

    Python/Log2Fibonacci: Add

---
+++ Python/Log2Fibonacci/main.py
@@ -0,0 +1,21 @@
+#! vim:fileencoding=utf-8
+
+def matrix2MulVec2(v0, m1):
+    return (v0[0] * m1[0][0] + v0[1] * m1[1][0], v0[0] * m1[0][1] + v0[1] * m1[1][1])
+def matrix2MulMatrix2(m0, m1):
+    return ((m0[0][0] * m1[0][0] + m0[0][1] * m1[1][0], m0[0][0] * m1[0][1] + m0[0][1] * m1[1][1]),
+            (m0[1][0] * m1[0][0] + m0[1][1] * m1[1][0], m0[1][0] * m1[0][1] + m0[1][1] * m1[1][1]))
+
+def _febn(v0, m0, n):
+    if n == 0:
+        return v0[1]
+    elif n % 2:
+        return _febn(matrix2MulVec2(v0, m0), m0, n - 1)
+    else:
+        return _febn(v0, matrix2MulMatrix2(m0, m0), n / 2)
+def febn(n):
+    return _febn((1, 0), ((1, 1), (1, 0)), n)
+
+for i in range(10):
+    print febn(i)
+print febn(1000)
