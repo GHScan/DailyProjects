55a0c96d3ec71cc66ecd2ecd83cc0a31e212e422
ghscan <ppscan@qq.com>
2014-05-08 18:44:25 +0800

    C++/C99ADT: Re-implemention linkedlist

--- C++/C99ADT/linkedList.c
+++ C++/C99ADT/linkedList.c
@@ -12,172 +12,154 @@ typedef struct Node_T {
 
 typedef struct LinkedList_T {
     const char *type;
-    Node_T *head, *tail;
+    Node_T *dummy;
     int size;
 } LinkedList_T;
 
 static Node_T* 
 createNode(void *value, Node_T *prev, Node_T *next) {
-    Node_T *n = malloc(sizeof(Node_T));
+    Node_T *n = malloc(sizeof(*n));
     n->value = value;
-    n->prev = prev;
     n->next = next;
+    n->prev = prev;
     return n;
+}
+
+static void 
+destroyNode(Node_T *n) {
+    free(n);
 }
 
 LinkedList_T*       
 LinkedList_create() {
-    LinkedList_T *o = malloc(sizeof(LinkedList_T));
+    LinkedList_T *o = malloc(sizeof(*o));
+
     o->type = TYPE;
-
-    o->head = createNode(0, 0, 0);
-    o->tail = o->head;
     o->size = 0;
+    o->dummy = createNode(0, 0, 0);
+    o->dummy->next = o->dummy->prev = o->dummy;
 
-    assert(o && o->type == TYPE);
     return o;
 }
 
 void                
 LinkedList_destroy(LinkedList_T **p) {
-    assert(p && *p && (*p)->type == TYPE);
+    assert(p != 0);
+    assert(*p != 0 && (*p)->type == TYPE);
 
-    LinkedList_T *o = *p;
-    for (Node_T *n = o->head, *next; n; n = next) {
+    for (Node_T *n = (*p)->dummy->next, *next; n != (*p)->dummy; n = next) {
         next = n->next;
-        free(n);
+        destroyNode(n);
     }
+    destroyNode((*p)->dummy);
+    free(*p);
 
-    free(o);
     *p = 0;
 }
 
 void                
 LinkedList_pushFront(LinkedList_T *o, void *p) {
-    assert(o && o->type == TYPE);
+    assert(o != 0 && o->type == TYPE);
 
-    Node_T *n = createNode(p, 0, o->head);
-    n->next->prev = n;
-    o->head = n;
-    ++o->size;
+    LinkedList_insert(o, LinkedList_begin(o), p);
 }
 
 void*               
 LinkedList_popFront(LinkedList_T *o) {
-    assert(o && o->type == TYPE);
-    assert(o->size > 0);
+    assert(o != 0 && o->type == TYPE && o->size > 0);
 
-    Node_T *n = o->head;
-    n->next->prev = 0;
-    o->head = n->next;
-
-    void *r = n->value;
-    free(n);
-    --o->size;
-
-    return r;
+    LinkedListIter_T *it = LinkedList_begin(o);
+    void* value = it->value;
+    LinkedList_remove(o, it);
+    return value;
 }
 
 void                
 LinkedList_pushBack(LinkedList_T *o, void *p) {
-    assert(o && o->type == TYPE);
+    assert(o != 0 && o->type == TYPE);
 
-    Node_T *tail = o->tail;
-    Node_T *n = createNode(p, tail->prev, tail);
-    if (tail->prev) tail->prev->next = n;
-    else o->head = n;
-    tail->prev = n;
-
-    ++o->size;
+    LinkedList_insert(o, LinkedList_end(o), p);
 }
 
 void*               
 LinkedList_popBack(LinkedList_T *o) {
-    assert(o && o->type == TYPE);
-    assert(o->size > 0);
+    assert(o != 0 && o->type == TYPE && o->size > 0);
 
-    Node_T *n = o->tail->prev;
-    if (n->prev) n->prev->next = n->next;
-    else o->head = n->next;
-    n->next->prev = n->prev;
-
-    void *r = n->value;
-    free(n);
-    --o->size;
-
-    return r;
+    LinkedListIter_T *it = (LinkedListIter_T*)o->dummy->prev;
+    void *value = it->value;
+    LinkedList_remove(o, it);
+    return value;
 }
 
 void*               
 LinkedList_front(LinkedList_T *o) {
-    assert(o && o->type == TYPE);
-    assert(o->size > 0);
+    assert(o != 0 && o->type == TYPE && o->size > 0);
 
-    return o->head->value;
+    return o->dummy->next->value;
 }
 
 void*               
 LinkedList_back(LinkedList_T *o) {
-    assert(o && o->type == TYPE);
-    assert(o->size > 0);
+    assert(o != 0 && o->type == TYPE && o->size > 0);
 
-   return o->tail->prev->value;
+    return o->dummy->prev->value;
 }
 
 int                 
 LinkedList_size(LinkedList_T *o) {
-    assert(o && o->type == TYPE);
+    assert(o != 0 && o->type == TYPE);
 
     return o->size;
 }
 
 LinkedListIter_T*   
 LinkedList_begin(LinkedList_T *o) {
-    assert(o && o->type == TYPE);
+    assert(o != 0 && o->type == TYPE);
 
-    return (LinkedListIter_T*)o->head;
+    return (LinkedListIter_T*)o->dummy->next;
 }
 
 LinkedListIter_T*   
 LinkedList_end(LinkedList_T *o) {
-    assert(o && o->type == TYPE);
+    assert(o != 0 && o->type == TYPE);
 
-    return (LinkedListIter_T*)o->tail;
+    return (LinkedListIter_T*)o->dummy;
 }
 
 LinkedListIter_T*   
 LinkedList_next(LinkedList_T *o, LinkedListIter_T *it) {
-    assert(o && o->type == TYPE);
+    assert(o != 0 && o->type == TYPE);
+    assert(it != 0);
 
     return (LinkedListIter_T*)((Node_T*)it)->next;
 }
 
 LinkedListIter_T*   
 LinkedList_insert(LinkedList_T *o, LinkedListIter_T *it, void *p) {
-    assert(o && o->type == TYPE);
+    assert(o != 0 && o->type == TYPE);
+    assert(it != 0);
 
-    Node_T *pos = (Node_T*)it;
-    Node_T *n = createNode(p, pos->prev, pos);
-    if (n->prev) n->prev->next = n;
-    else o->head = n;
-    n->next->prev = n;
+    Node_T *n = (Node_T*)it;
+    Node_T *prev = n->prev;
+    Node_T *newN = createNode(p, prev, n);
+    prev->next = n->prev = newN;
     ++o->size;
 
-    return (LinkedListIter_T*)n;
+    return (LinkedListIter_T*)newN;
 }
 
 LinkedListIter_T*   
 LinkedList_remove(LinkedList_T *o, LinkedListIter_T *it) {
-    assert(o && o->type == TYPE);
+    assert(o != 0 && o->type == TYPE && o->size > 0);
+    assert(it != 0);
 
     Node_T *n = (Node_T*)it;
-    if (n->prev) n->prev->next = n->next;
-    else o->head = n->next;
-    n->next->prev = n->prev;
+    Node_T *prev = n->prev, *next = n->next;
+    prev->next = next;
+    next->prev = prev;
 
-    Node_T *r = n->next;
-    free(n);
+    destroyNode(n);
     --o->size;
 
-    return (LinkedListIter_T*)r;
+    return (LinkedListIter_T*)next;
 }
