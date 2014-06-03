587b69a6413783ce7e81735fe19654323d7f030f
GHScan <ppscan@qq.com>
2014-06-03 14:53:04 +0800

    Lua/SchemeInterpreter/2_LambdaCalculus: Refactor, remove the argument 'vm' of S_interpret

--- Lua/SchemeInterpreter/2_LambdaCalculus/main.lua
+++ Lua/SchemeInterpreter/2_LambdaCalculus/main.lua
@@ -5,32 +5,27 @@ function S_parse(s)
     s = string.gsub(s, '[^{},%d][^{},]*', '"%1"')
     return assert(loadstring(string.format("return {%s}", s)))()[1]
 end
-function S_lookupVar(vm, env, name)
-    while env do
-        if env[name] then return env[name] end
-        env = env[vm]
-    end
-end
-function S_createLambda(vm, env, argIdx, expArgs, expBody)
+function S_createLambda(env, argIdx, expArgs, expBody)
     return function(arg)
-        local newEnv = {[vm]=env, [expArgs[argIdx]]=arg}
+        local newEnv = {[expArgs[argIdx]]=arg}
+        setmetatable(newEnv, {__index=env})
         if argIdx == #expArgs then
-            for i = 3, #expBody - 1 do S_interpret(vm, newEnv, expBody[i]) end
-            return S_interpret(vm, newEnv, expBody[#expBody])
+            for i = 3, #expBody - 1 do S_interpret(newEnv, expBody[i]) end
+            return S_interpret(newEnv, expBody[#expBody])
         else
-            return S_createLambda(vm, newEnv, argIdx + 1, expArgs, expBody)
+            return S_createLambda(newEnv, argIdx + 1, expArgs, expBody)
         end
     end
 end
-function S_interpret(vm, env, exp)
+function S_interpret(env, exp)
     if type(exp) == 'string' then 
-        return S_lookupVar(vm, env, exp) 
+        return env[exp]
     elseif exp[1] == 'lambda' then
-        return S_createLambda(vm, env, 1, #exp[2] > 0 and exp[2] or {'_'}, exp)
+        return S_createLambda(env, 1, #exp[2] > 0 and exp[2] or {'_'}, exp)
     else 
-        local p = S_interpret(vm, env, exp[1])
+        local p = S_interpret(env, exp[1])
         for i = 2, math.max(#exp, 2) do 
-            p = p(exp[i] and S_interpret(vm, env, exp[i]) or nil)
+            p = p(exp[i] and S_interpret(env, exp[i]) or nil)
         end
         return p
     end
@@ -43,7 +38,7 @@ function S_createVM()
     }
 end
 function S_eval(vm, s)
-    return S_interpret(vm, vm.G, S_parse(s))
+    return S_interpret(vm.G, S_parse(s))
 end
 
 S_eval(S_createVM(), io.read('*a'))
