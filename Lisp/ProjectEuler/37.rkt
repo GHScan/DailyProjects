ea7897f5acc300738289d56a739fb982e33249cf
GHScan <ppscan@qq.com>
2014-06-06 03:15:58 +0800

    Lisp/ProjectEuler: Refactor 37.rkt

--- Lisp/ProjectEuler/37.rkt
+++ Lisp/ProjectEuler/37.rkt
@@ -12,21 +12,23 @@
     digits)
   )
 
+;------------------------------
+(define left-expander (lambda (prime digit) (cons digit prime)))
+(define right-expander (lambda (prime digit) (append prime (list digit))))
+(define left-digits (range 1 10))
+(define right-digits '(1 3 5 7 9))
 (define init-primes '((2)(3)(5)(7)))
 (define truncatable-primes
   (let iter ([left-primes init-primes][right-primes init-primes]
-             [result empty]
-             [left-expander (lambda (prime digit) (cons digit prime))][right-expander (lambda (prime digit) (append prime (list digit)))]
-             [left-digits (range 1 10)][right-digits '(1 3 5 7 9)])
-    (if (or (empty? left-primes) (empty? right-primes) (>= (length result) 15))
+             [result empty])
+    (if (>= (length result) 15)
       result
       (iter 
         (expand-primes left-primes left-expander left-digits) 
         (expand-primes right-primes right-expander right-digits) 
         (append (list-intersect (map list->number left-primes)
                                 (map list->number right-primes))
-                result)
-        left-expander right-expander left-digits right-digits))
+                result)))
     ))
 
 (apply + (filter (curry < 10) truncatable-primes))
