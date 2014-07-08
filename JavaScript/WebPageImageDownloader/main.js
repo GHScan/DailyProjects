557e445d75c78fea6b93ffa3826e8a73da247587
GHScan <ppscan@qq.com>
2014-07-08 20:42:15 +0800

    Javascript/WebPageImageDownloader: Bugfix, binary file writing

--- JavaScript/WebPageImageDownloader/main.js
+++ JavaScript/WebPageImageDownloader/main.js
@@ -8,21 +8,20 @@ var util = require('util');
 
 var jsdom = require('jsdom');
 //------------------------------
-
-function downloadUrl(dir, fileUrl) {
+function downloadUrl(dir, fileUrl, id) {
     if (!fs.existsSync(dir)) fs.mkdirSync(dir);
 
     http.get(fileUrl, function(res) {
 
-        var data = '';
+        var chunks = [];
         res.on('data', function(chunk) {
-            data += chunk;
+            chunks.push(chunk);
         });
         res.on('end', function() {
-            var localPath = path.join(dir, path.basename(url.parse(fileUrl).pathname));
+            var localPath = path.join(dir, id + '_' + path.basename(url.parse(fileUrl).pathname));
             console.log(util.format('@ [%s] downloaded: %s => %s', new Date().toLocaleTimeString(), fileUrl, localPath));
 
-            fs.writeFileSync(localPath, data);
+            fs.writeFileSync(localPath, Buffer.concat(chunks), 'binary');
         });
 
     }).on('error', function(e) {
@@ -36,14 +35,25 @@ jsdom.env(
         function (errors, window) {
 
             var suffix = ['.png', '.jpg'];
-            var urls = {};
+            var url2Id = {};
+            var id = 0;
             for (var i = 0; i < suffix.length; ++i) {
                 var elems = window.$(util.format('[src$="%s"]', suffix[i]));
-                for (var j = 0; j < elems.length; ++j) urls[elems[j].src] = true;
+                for (var j = 0; j < elems.length; ++j) {
+                    if (!url2Id.hasOwnProperty(elems[j].src)) {
+                        url2Id[elems[j].src] = id++;
+                    }
+                }
             }
 
-            for (var url in urls) {
-                downloadUrl('files', url);
+            var urlArray = [];
+            for (var url in url2Id) {
+                urlArray.push({url:url, id:url2Id[url]});
+            }
+            urlArray.sort(function(a, b){ return a.id - b.id; });
+
+            for (var i in urlArray) {
+                downloadUrl('files', urlArray[i].url, urlArray[i].id);
             }
             console.log('@ finish...');
         }
