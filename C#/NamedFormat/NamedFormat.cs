8638f7778ddc7491ae2e316aba80633c22050078
GHScan <ppscan@qq.com>
2014-11-23 22:04:07 +0800

    C#/NamedFormat: Bugfix

--- C#/NamedFormat/NamedFormat.cs
+++ C#/NamedFormat/NamedFormat.cs
@@ -9,19 +9,14 @@ using System.Collections.Concurrent;
 using System.Text.RegularExpressions;
 using System.Linq.Expressions;
 
-namespace CSharp13
-{
-    public class Program
-    {
-        public static class NamedFormat
-        {
+namespace CSharp13 {
+    public class Program {
+        public static class NamedFormat {
             private static readonly Regex NameParseRegex = new Regex(@"(?<!{){(\w+)(:[^}]+)?}(?!})", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.Multiline);
 
-            public static class FormatterCache<T>
-            {
+            public static class FormatterCache<T> {
                 public static readonly ConcurrentDictionary<string, Func<T, string>> Cache = new ConcurrentDictionary<string, Func<T, string>>();
-                public static Func<T, string> GetFormatter(string formatStr)
-                {
+                public static Func<T, string> GetFormatter(string formatStr) {
                     Func<T, string> formatter;
                     if (Cache.TryGetValue(formatStr, out formatter)) return formatter;
 
@@ -38,68 +33,62 @@ namespace CSharp13
                     Cache.TryAdd(formatStr, formatter);
                     return formatter;
                 }
-                private static string NormalizeFormatString(string formatStr, out string[] fieldNames)
-                {
-                    var nameSet = new HashSet<string>();
-                    foreach (Match m in NameParseRegex.Matches(formatStr))
-                    {
-                        nameSet.Add(m.Groups[1].Value);
-                    }
-                    fieldNames = nameSet.ToArray();
-
-                    for (var i = 0; i < fieldNames.Length; ++i)
-                    {
-                        formatStr = formatStr.Replace(fieldNames[i], i.ToString());
-                    }
-
-                    return formatStr;
+                private static string NormalizeFormatString(string formatStr, out string[] fieldNames) {
+                    var name2Idx = new Dictionary<string, int>();
+                    var result = NameParseRegex.Replace(formatStr, m => { 
+                        string name = m.Groups[1].Value;
+                        int idx;
+                        if (!name2Idx.TryGetValue(name, out idx)) { 
+                            idx = name2Idx.Count;
+                            name2Idx.Add(name, idx);
+                        }
+                        return string.Format("{{{0}{1}}}", idx, m.Groups[2].Value);
+                    });
+                    fieldNames = name2Idx.OrderBy(kv => kv.Value).Select(kv => kv.Key).ToArray();
+                    return result;
                 }
             }
 
-            public static string Format<T>(string formatStr, T obj)
-            {
+            public static string Format<T>(string formatStr, T obj) {
                 return FormatterCache<T>.GetFormatter(formatStr)(obj);
             }
         }
-        public static class NamedFormatForDictionary
-        {
+        public static class NamedFormatForDictionary {
             private static readonly Regex NameParseRegex = new Regex(@"(?<!{){(\w+)(:[^}]+)?}(?!})", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.Multiline);
-            private class FormatStringMeta
-            { 
+            private class FormatStringMeta {
                 public string[] Names;
                 public object[] TempValues;
                 public string NormalizedFormatStr;
             }
             private static readonly Dictionary<string, FormatStringMeta> FormatStrMetaCache = new Dictionary<string, FormatStringMeta>();
-            private static FormatStringMeta ParseFormatString(string formatStr)
-            {
-                var nameSet = new HashSet<string>();
-                foreach (Match m in NameParseRegex.Matches(formatStr))
-                {
-                    nameSet.Add(m.Groups[1].Value);
-                }
-                var names = nameSet.ToArray();
+            private static FormatStringMeta ParseFormatString(string formatStr) {
+                var name2Idx = new Dictionary<string, int>();
+                string result = NameParseRegex.Replace(formatStr, m => { 
+                    var name = m.Groups[1].Value;
+                    int idx;
+                    if (!name2Idx.TryGetValue(name, out idx)) { 
+                        idx = name2Idx.Count;
+                        name2Idx.Add(name, idx);
+                    }
+                    return string.Format("{{{0}{1}}}", idx, m.Groups[2].Value);
+                });
 
-                for (var i = 0; i < names.Length; ++i)
-                {
-                    formatStr = formatStr.Replace(names[i], i.ToString());
-                }
-
-                return new FormatStringMeta { Names = names, TempValues = new object[names.Length], NormalizedFormatStr = formatStr };
+                return new FormatStringMeta {
+                    Names = name2Idx.OrderBy(kv => kv.Value).Select(kv => kv.Key).ToArray(),
+                    TempValues = new object[name2Idx.Count],
+                    NormalizedFormatStr = result
+                };
             }
-            public static string Format<T>(string formatStr, IDictionary<string, T> dict)
-            {
+            public static string Format<T>(string formatStr, IDictionary<string, T> dict) {
                 FormatStringMeta meta;
-                if (!FormatStrMetaCache.TryGetValue(formatStr, out meta))
-                { 
+                if (!FormatStrMetaCache.TryGetValue(formatStr, out meta)) {
                     meta = ParseFormatString(formatStr);
                     FormatStrMetaCache.Add(formatStr, meta);
                 }
 
                 var names = meta.Names;
                 var values = meta.TempValues;
-                for (var i = 0; i < names.Length; ++i)
-                { 
+                for (var i = 0; i < names.Length; ++i) {
                     values[i] = dict[names[i]];
                 }
 
@@ -107,22 +96,19 @@ namespace CSharp13
             }
         }
 
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) {
             const int N = 1000;
             string formatStr = "{Name} is {Age:0.0} years old";
             // string formatStr = "{Name} is {Age:0.0} years old, {Name} is {Age:0.000} years old, {Name} is {Age:0.####} years old";
 
             {
                 string text = null;
-                var input = new
-                {
+                var input = new {
                     Name = "Santa",
                     Age = 1700.25
                 };
 
-                Utils.Timeit(N, () =>
-                {
+                Utils.Timeit(N, () => {
                     text = NamedFormat.Format(formatStr, input);
                 });
 
@@ -137,9 +123,18 @@ namespace CSharp13
                     {"Age" , 1700.25}
                 };
 
-                Utils.Timeit(N, () =>
-                {
+                Utils.Timeit(N, () => {
                     text = NamedFormatForDictionary.Format(formatStr, input);
+                });
+
+                Console.WriteLine(text);
+            }
+
+            {
+                string text = null;
+
+                Utils.Timeit(N, () => {
+                    text = string.Format("{0} is {1:0.0} years old", "Santa", 1700.25);
                 });
 
                 Console.WriteLine(text);
