d3af52b34eebc73c417e6a4f6fc3d202d71c81b7
ghscan <wyyxscan@126.com>
2014-11-29 17:42:58 +0800

    Scala/Sort: Add GC status support

--- Scala/Sort/main.scala
+++ Scala/Sort/main.scala
@@ -1,7 +1,7 @@
 import scala.util.Random
 
 object Test extends App {
-	override def main(args : Array[String]) {
+		override def main(args : Array[String]) {
 		val len = 1024 * 1024
 
 		val randList = List.fill(len)(Random.nextInt)
@@ -129,8 +129,32 @@ object Test extends App {
 
 	def timeit(times : Int, f : () => Unit) {
 		if (times > 1) f()
-		val start = System.nanoTime()
+		val start = System.nanoTime
+		val gcStatus = GC.status
 		for (_ <- 0 until times) f()
-		println((System.nanoTime() - start) / 1000000000.0 / times)
+		println((System.nanoTime - start) / 1000000000.0 / times, GC.status - gcStatus)
+	}
+
+	class GCStatus(val status : List[(String, Long, Float)]) {
+		def -(o : GCStatus) =
+			new GCStatus(status.map(s =>
+				o.status.find(v => v._1 == s._1) match {
+					case None => s
+					case Some((oname, ocount, otime)) => (oname, s._2 - ocount, s._3 - otime)
+				}).filter(s=>s._2 > 0))
+		override def toString = "GC:" + 
+				status
+				.map(v => f"(${v._1}%s,count=${v._2}%d,time=${v._3}%.3f)")
+				.fold("")((a, b) => a + b) 
+	}
+	object GC {
+		def status =
+			new GCStatus(
+				java.lang.management.ManagementFactory.getGarbageCollectorMXBeans
+					.toArray
+					.map(o => o.asInstanceOf[java.lang.management.GarbageCollectorMXBean])
+					.filter(g => g.getCollectionCount > 0)
+					.map(g => (g.getName, g.getCollectionCount, g.getCollectionTime / 1000.0f))
+					.toList)
 	}
 }
