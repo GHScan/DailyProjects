99b0a55ba3d119c59841837a3d4a4986c6750575
GHScan <ppscan@qq.com>
2014-12-10 23:26:08 +0800

    Scala/Sort : Add mergeSort

--- Scala/Sort/main.scala
+++ Scala/Sort/main.scala
@@ -14,7 +14,10 @@ object Test extends App {
       qsort_g(randList)
     }
     Utils.timeit("qsort")(kTimes) {
-      qsort(randList)(_ < _)
+      qsort[Int](randList, _ < _)
+    }
+    Utils.timeit("mergeSort")(kTimes) {
+      mergeSort[Int](randList, _ < _)
     }
 
     val randArrays = List.fill(kTimes + 1) { Array.fill(kLen)(Random.nextInt) }
@@ -35,7 +38,7 @@ object Test extends App {
     tempArrays = randArrays.map(a => a.clone).iterator
     Utils.timeit("qsort2_g")(kTimes) {
       val a = tempArrays.next
-      qsort2_g[Int](a, 0, a.length)
+      qsort2_g(a, 0, a.length)
     }
     tempArrays = randArrays.map(a => a.clone).iterator
     Utils.timeit("qsort3")(kTimes) {
@@ -45,7 +48,12 @@ object Test extends App {
     tempArrays = randArrays.map(a => a.clone).iterator
     Utils.timeit("qsort3_g")(kTimes) {
       val a = tempArrays.next
-      qsort3_g[Int](a, 0, a.length)
+      qsort3_g(a, 0, a.length)
+    }
+    tempArrays = randArrays.map(a => a.clone).iterator
+    Utils.timeit("mergeSort_2")(kTimes) {
+      val a = tempArrays.next
+      mergeSort_2[Int](a, _ < _)
     }
   }
 
@@ -58,11 +66,26 @@ object Test extends App {
       case head :: tail => qsort_g(tail.filter(_ < head)) ++: List(head) ++: qsort_g(tail.filter(_ > head))
     }
   }
-  def qsort[T](a : List[T])(implicit f : (T, T) => Boolean) : List[T] = {
+  def qsort[T](a : List[T], f : (T, T) => Boolean) : List[T] = {
     a match {
       case Nil => Nil
-      case head :: tail => qsort(tail.filter(f(_, head))) ::: head :: qsort(tail.filter(f(head, _)))
+      case head :: tail => qsort(tail.filter(f(_, head)), f) ::: head :: qsort(tail.filter(f(head, _)), f)
     }
+  }
+  def mergeSort[T](a : List[T], f : (T, T) => Boolean) : List[T] = {
+    def merge(a : List[T], b : List[T], result : List[T]) : List[T] = {
+      if (a.isEmpty) result.reverse ::: b
+      else if (b.isEmpty) result.reverse ::: a
+      else if (f(a.head, b.head)) merge(a.tail, b, a.head :: result)
+      else merge(a, b.tail, b.head :: result)
+    }
+    def sort(a : List[T], len : Int) : List[T] = {
+      if (len < 2) return a
+      val half = len / 2
+      val (left, right) = a.splitAt(half)
+      merge(sort(left, half), sort(right, len - half), Nil)
+    }
+    sort(a, a.length)
   }
   def qsort1(a : Array[Int], begin : Int, end : Int) {
     if (end - begin <= 1) return
@@ -105,7 +128,7 @@ object Test extends App {
   }
   def qsort2_g[T](a : mutable.Seq[T], begin : Int, end : Int)(implicit ord : Ordering[T]) {
     import ord._
-    
+
     if (end - begin <= 1) return
 
     val head = a(begin)
@@ -173,7 +196,7 @@ object Test extends App {
   }
   def qsort3_g[T](a : mutable.Seq[T], begin : Int, end : Int)(implicit ord : Ordering[T]) {
     import ord._
-    
+
     (end - begin) match {
       case 0 => return
       case 1 => return
@@ -226,5 +249,61 @@ object Test extends App {
       a(j) = iv
       a(i) = jv
     }
+  }
+
+  def mergeSort_2[@specialized(Int) T : Manifest](a : Array[T], f : (T, T) => Boolean) {
+    def merge(
+      to : Array[T], toBegin : Int,
+      from1 : Array[T], from1Begin : Int, len1 : Int,
+      from2 : Array[T], from2Begin : Int, len2 : Int) {
+      if (len1 == 0) {
+        var i = 0
+        while (i < len2) {
+          to(toBegin + i) = from2(from2Begin + i)
+          i += 1
+        }
+      } else if (len2 == 0) {
+        var i = 0
+        while (i < len1) {
+          to(toBegin + i) = from1(from1Begin + i)
+          i += 1
+        }
+      } else {
+        if (f(from1(from1Begin), from2(from2Begin))) {
+          to(toBegin) = from1(from1Begin)
+          merge(to, toBegin + 1, from1, from1Begin + 1, len1 - 1, from2, from2Begin, len2)
+        } else {
+          to(toBegin) = from2(from2Begin)
+          merge(to, toBegin + 1, from1, from1Begin, len1, from2, from2Begin + 1, len2 - 1)
+        }
+      }
+    }
+
+    def sort(a : Array[T], begin : Int, len : Int, temp : Array[T], tempBegin : Int) {
+      if (len < 2) return
+      
+      val half = len / 2
+      val mid = begin + half
+      sortTo(a, begin, half, temp, tempBegin)
+      sortTo(a, mid, len - half, temp, tempBegin + half)
+      merge(a, begin, temp, tempBegin, half, temp, tempBegin + half, len - half)
+    }
+    def sortTo(a : Array[T], begin : Int, len : Int, temp : Array[T], tempBegin : Int) {
+      if (len < 2) {
+        if (len == 1) {
+          temp(tempBegin) = a(begin)
+        }
+        return
+      }
+
+      val half = len / 2
+      val mid = begin + half
+      sort(a, begin, half, temp, tempBegin)
+      sort(a, mid, len - half, temp, tempBegin)
+      merge(temp, tempBegin, a, begin, half, a, mid, len - half)
+    }
+
+    val temp = new Array[T](a.length)
+    sort(a, 0, a.length, temp, 0)
   }
 }
