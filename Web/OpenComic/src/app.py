1de0760198d9fd349ef994ffffa48a450511fc69
GHScan <ppscan@qq.com>
2016-04-04 21:02:34 +0800

    Web/OpenComic: Default loading image; Set session expires to 14 days

--- Web/OpenComic/src/app.py
+++ Web/OpenComic/src/app.py
@@ -1,11 +1,17 @@
 # vim:fileencoding=utf-8
 
-from flask import Flask
+from datetime import timedelta
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 import constants
 
 flask = Flask(__name__)
 flask.config['SQLALCHEMY_DATABASE_URI'] = constants.DB_FILE_URI
 flask.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+flask.config['PERMANENT_SESSION_LIFETIME '] = timedelta(14)
+
+@flask.before_request
+def make_session_permanent():
+    session.permanent = True
 
 db = SQLAlchemy(flask)
