e4a985ceb9f3fd89568e0d6c638c1b0ff443c785
GHScan <wyyxscan@126.com>
2017-07-25 16:12:59 +0800

    C++/NewtonRaphsonDivision: Fix more corner cases

--- C++/ieee浮点数格式验证/NewtonRaphsonDivision.cpp
+++ C++/ieee浮点数格式验证/NewtonRaphsonDivision.cpp
@@ -1,41 +1,67 @@
+#include "stdafx.h"
 
 #include <cstdint>
+#include <cstdio>
 
 template<typename TDest, typename TSrc>
 inline TDest UnsafeCast(TSrc v) {
     union {
         TSrc src;
         TDest dest;
-    } u {v};
+    } u{ v };
     return u.dest;
 }
 
-/*
-    Newton–Raphson division:
-    https://en.wikipedia.org/wiki/Division_algorithm#Newton.E2.80.93Raphson_division
- */
-static float div(float n, float d) {    
-    uint32_t e_minus1 = 126 << 23;
-    uint32_t e_d = UnsafeCast<uint32_t>(d) & (0xff << 23);
-    uint32_t e_n = UnsafeCast<uint32_t>(n) & (0xff << 23);
-    uint32_t e_n2 = (e_n + e_minus1 - e_d) & (0xff << 23);
+constexpr uint32_t Log2(uint32_t n) {
+    return n == 0 ? 0 : 1 + Log2(n >> 1);
+}
 
-    auto d2 = UnsafeCast<float>((UnsafeCast<uint32_t>(d) & ~(0xff << 23)) | e_minus1);
-    auto n2 = UnsafeCast<float>((UnsafeCast<uint32_t>(n) & ~(0xff << 23)) | e_n2);
+static float Divide(float n, float d) {
+    uint32_t constexpr s_mask = 1 << 31;
+    uint32_t constexpr e_mask = 0xff << 23;
+    uint32_t constexpr s_e_mask = s_mask | e_mask;
+    uint32_t constexpr e_minus1 = 126 << 23;
+
+
+    uint32_t s_d = UnsafeCast<uint32_t>(d) & s_mask;
+    uint32_t e_d = UnsafeCast<uint32_t>(d) & e_mask;
+    uint32_t e_n = UnsafeCast<uint32_t>(n) & e_mask;
+    uint32_t e_shift = e_minus1 - e_d;
+    uint32_t e_n2 = e_n + e_shift;
+
+    auto d2 = UnsafeCast<float>((UnsafeCast<uint32_t>(d) & ~s_e_mask) | e_minus1);
+    float n2;
+    if (e_n2 & s_mask) {
+        if (e_shift & s_mask)   // underflow
+            n2 = UnsafeCast<float>((UnsafeCast<uint32_t>(n) & s_mask) ^ s_d);
+        else                    // overflow
+            n2 = UnsafeCast<float>((UnsafeCast<uint32_t>(n) & s_mask | e_mask) ^ s_d);
+    } else {
+        n2 = UnsafeCast<float>(((UnsafeCast<uint32_t>(n) & ~e_mask) | e_n2) ^ s_d);
+    }
+
+
+    uint32_t constexpr precision = 23;
+    uint32_t constexpr steps = Log2((precision + Log2(17)) / Log2(17));
 
     float x = 48 / 17.0 - 32 / 17.0 * d2;
-    for (auto _ = 0; _ < 3; ++_) {
+    for (auto _ = 0; _ < steps; ++_) {
         x = x + x * (1 - d2 * x);
     }
+
 
     return n2 * x;
 }
 
 int main() {
-    float numbers[] = { 1e-20, 0.1, 0.2, 0.7, 0.9, 1, 2, 4, 9, 300, 50000, 300000, 1e20 };
+    float numbers[] = { -3e-22, 2e-10, 0.1, 0.6, 1, 7, 200, 300000, 5e8, -7e20 };
     for (auto a : numbers) {
         for (auto b : numbers) {
-            printf("%f/%f->%f (%f)\n", a, b, div(a, b), a / b);
+            auto c = Divide(a, b);
+            auto d = a / b;
+            if (c != d) {
+                printf("%f/%f->%f (%f)\n", a, b, c, d);
+            }
         }
     }
 }
