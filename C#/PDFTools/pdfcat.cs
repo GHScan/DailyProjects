eb1ec81251968f0b79f760d119d72dcc510de447
GHScan <wyyxscan@126.com>
2017-10-27 18:50:46 +0800

    C#/PDFTools: Add

---
+++ C#/PDFTools/pdfcat.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace CSharp2015
+{
+    public class Program
+    {
+        private static void MergePdfs(string outputPath, IEnumerable<string> pdfPaths)
+        {
+            using (var outputDoc = new PdfDocument())
+            {
+                var title2page = new Dictionary<string, int>();
+
+                foreach (var path in pdfPaths)
+                {
+                    title2page.Add(Path.GetFileNameWithoutExtension(path), outputDoc.PageCount + 2);
+
+                    using (var fileDoc = PdfReader.Open(path, PdfDocumentOpenMode.Import))
+                    {
+                        var pageCount = fileDoc.PageCount;
+                        for (var i = 0; i < pageCount; i++)
+                        {
+                            outputDoc.AddPage(fileDoc.Pages[i]);
+                        }
+                    }
+                }
+
+                var contentPage = outputDoc.InsertPage(0);
+                for (var fontSize = 50; fontSize >= 10; fontSize -= 5)
+                {
+                    try
+                    {
+                        RenderContentPage(contentPage, title2page, fontSize);
+                        break;
+                    }
+                    catch
+                    {
+                    }
+                }
+
+                outputDoc.Save(outputPath);
+            }
+        }
+
+        private static void RenderContentPage(PdfPage page, IDictionary<string, int> title2page, int frontSize)
+        {
+            var font = new XFont("Monaco", frontSize, XFontStyle.Bold, 
+                    new XPdfFontOptions(PdfFontEncoding.Unicode, PdfFontEmbedding.Always));
+            var brush = XBrushes.Black;
+            var pen = new XPen(brush.Color) { DashStyle = XDashStyle.Dot };
+            var border = 4;
+
+            using (var g = XGraphics.FromPdfPage(page))
+            {
+                var maxWidth = .0;
+                var totalHeight = .0;
+                var height = .0;
+                foreach (var kv in title2page)
+                {
+                    var size = g.MeasureString(kv.Key, font);
+                    maxWidth = Math.Max(size.Width, maxWidth);
+                    height = size.Height;
+                    totalHeight += height + border;
+                }
+
+                var x = (page.Width.Point - maxWidth) / 2;
+                var y = (page.Height.Point - totalHeight) / 2;
+                if (x < 0 || y < 0) throw new ArgumentException("invalid font size");
+
+                foreach (var kv in title2page.OrderBy(kv => kv.Key))
+                {
+                    var rect = new XRect(new XPoint(x, y), new XSize(maxWidth, height));
+
+                    g.DrawString(kv.Key, font, brush, rect, XStringFormats.TopLeft);
+                    g.DrawRectangle(pen, rect);
+                    page.AddDocumentLink(new PdfRectangle(g.Transformer.WorldToDefaultPage(rect)), kv.Value);
+
+                    y += height + border;
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("pdfcat.exe pdfDir");
+                return;
+            }
+
+            MergePdfs("cat.pdf", Directory.GetFiles(args[0]));
+        }
+    }
+}
